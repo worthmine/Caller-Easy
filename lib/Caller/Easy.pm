a4fcd94610728c27ba878336853098c8c41e61af
worthmine <worthmine@gmail.com>
2020-08-01 14:35:23 +0900

    just optimized for readability

--- lib/Caller/Easy.pm
+++ lib/Caller/Easy.pm
@@ -6,70 +6,70 @@ use Carp;
 
 our $VERSION = "0.02";
 
-use overload '""' => sub{ $_[0]->package() }, fallback => 1;
+use overload '""' => sub { $_[0]->package() }, fallback => 1;
 
 use Moose;
 
-has 'depth'         => ( is => 'ro',                                isa => 'Maybe[Num]' );
-has 'package'       => ( is => 'ro', writer => '_set_package',      isa => 'Str' );
-has 'filename'      => ( is => 'ro', writer => '_set_filename',     isa => 'Str' );
-has 'line'          => ( is => 'ro', writer => '_set_line',         isa => 'Num' );
-has 'subroutine'    => ( is => 'ro', writer => '_set_subroutine',   isa => 'Str' );
-has 'hasargs'       => ( is => 'ro', writer => '_set_hasargs',      isa => 'Bool' );
-has 'wantarray'     => ( is => 'ro', writer => '_set_wantarray',    isa => 'Bool' );
-has 'evaltext'      => ( is => 'ro', writer => '_set_evaltext',     isa => 'Str' );
-has 'is_require'    => ( is => 'ro', writer => '_set_is_require',   isa => 'Bool' );
-has 'hints'         => ( is => 'ro', writer => '_set_hints',        isa => 'Num' );
-has 'bitmask'       => ( is => 'ro', writer => '_set_bitmask',      isa => 'Str' );
-has 'hinthash'      => ( is => 'ro', writer => '_set_hinthash',     isa => 'Maybe[HashRef]' );
-has 'args'          => ( is => 'ro', writer => '_set_args',         isa => 'Maybe[ArrayRef]' );
+has 'depth'      => ( is => 'ro', isa    => 'Maybe[Num]' );
+has 'package'    => ( is => 'ro', writer => '_set_package', isa => 'Str' );
+has 'filename'   => ( is => 'ro', writer => '_set_filename', isa => 'Str' );
+has 'line'       => ( is => 'ro', writer => '_set_line', isa => 'Num' );
+has 'subroutine' => ( is => 'ro', writer => '_set_subroutine', isa => 'Str' );
+has 'hasargs'    => ( is => 'ro', writer => '_set_hasargs', isa => 'Bool' );
+has 'wantarray'  => ( is => 'ro', writer => '_set_wantarray', isa => 'Bool' );
+has 'evaltext'   => ( is => 'ro', writer => '_set_evaltext', isa => 'Str' );
+has 'is_require' => ( is => 'ro', writer => '_set_is_require', isa => 'Bool' );
+has 'hints'      => ( is => 'ro', writer => '_set_hints', isa => 'Num' );
+has 'bitmask'    => ( is => 'ro', writer => '_set_bitmask', isa => 'Str' );
+has 'hinthash'   => ( is => 'ro', writer => '_set_hinthash', isa => 'Maybe[HashRef]' );
+has 'args'       => ( is => 'ro', writer => '_set_args', isa => 'Maybe[ArrayRef]' );
 
 around BUILDARGS => sub {
     my $orig  = shift;
     my $class = shift;
 
-    if( @_ == 1 ) {
+    if ( @_ == 1 ) {
         return $class->$orig( depth => $_[0] ) if $_[0] =~ /^\d+$/;
         croak 'Unvalid depth was assigned';
-    }elsif( @_ > 2 ) {
+    }
+    elsif ( @_ > 2 ) {
         croak 'Too many arguments for caller';
-    }elsif( @_ == 2 and not exists $_{depth} ) {
+    }
+    elsif ( @_ == 2 and not exists $_{depth} ) {
         croak 'Unvalid arguments for caller';
-    }else{
+    }
+    else {
         return $class->$orig(@_);
     }
 };
 
 sub BUILD {
-    my $self = shift;
+    my $self  = shift;
     my $depth = $self->depth();
 
-    my (
-        $package, $filename, $line,
-        $subroutine, $hasargs, $wantarray, $evaltext,
-        $is_require, $hints, $bitmask, $hinthash
+    my ($package,  $filename,   $line,  $subroutine, $hasargs, $wantarray,
+        $evaltext, $is_require, $hints, $bitmask,    $hinthash
     );
 
+    if ( defined $depth and $depth =~ /^\d+$/ ) {
 
-    if( defined $depth and $depth =~ /^\d+$/ ) {
         package DB {
             our @args;
             my $i = 1;
             do {
-                ( $package, $filename, $line ) = CORE::caller($i++);
-            } while( $package =~ /^Test::/ or $package =~ /^Caller::Easy/ );
+                ( $package, $filename, $line ) = CORE::caller( $i++ );
+            } while ( $package =~ /^Test::/ or $package =~ /^Caller::Easy/ );
 
-            (
-                undef, undef, undef,
-                $subroutine, $hasargs, $wantarray, $evaltext,
-                $is_require, $hints, $bitmask, $hinthash
+            (   undef,     undef,       undef,  $subroutine, $hasargs, $wantarray,
+                $evaltext, $is_require, $hints, $bitmask,    $hinthash
             ) = CORE::caller( $depth + $i++ );
         }
-    }else{
+    }
+    else {
         my $i = 1;
         do {
-            ( $package, $filename, $line ) = CORE::caller($i++);
-        } while( $package =~ /^Test::/ or $package =~ /^Caller::Easy/ );
+            ( $package, $filename, $line ) = CORE::caller( $i++ );
+        } while ( $package =~ /^Test::/ or $package =~ /^Caller::Easy/ );
 
         $self->_set_package($package)   if $package;
         $self->_set_filename($filename) if $filename;
@@ -79,24 +79,23 @@ sub BUILD {
         return ( $package, $filename, $line );
     }
 
-    $self->_set_package($package)           if $package;
-    $self->_set_filename($filename)         if $filename;
-    $self->_set_line($line)                 if $line;
-    $self->_set_subroutine($subroutine)     if $subroutine;
-    $self->_set_hasargs($hasargs)           if defined $hasargs;
-    $self->_set_wantarray($wantarray)       if defined $wantarray;
-    $self->_set_evaltext($evaltext)         if $evaltext;
-    $self->_set_is_require($is_require)     if defined $is_require;
-    $self->_set_hints($hints)               if $hints;
-    $self->_set_bitmask($bitmask)           if $bitmask;
-    $self->_set_hinthash($hinthash)         if $hinthash;
-    $self->_set_args(\@DB::args);
+    $self->_set_package($package)       if $package;
+    $self->_set_filename($filename)     if $filename;
+    $self->_set_line($line)             if $line;
+    $self->_set_subroutine($subroutine) if $subroutine;
+    $self->_set_hasargs($hasargs)       if defined $hasargs;
+    $self->_set_wantarray($wantarray)   if defined $wantarray;
+    $self->_set_evaltext($evaltext)     if $evaltext;
+    $self->_set_is_require($is_require) if defined $is_require;
+    $self->_set_hints($hints)           if $hints;
+    $self->_set_bitmask($bitmask)       if $bitmask;
+    $self->_set_hinthash($hinthash)     if $hinthash;
+    $self->_set_args( \@DB::args );
 
     return $self unless wantarray;
     return (
-        $package, $filename, $line,
-        $subroutine, $hasargs, $wantarray, $evaltext,
-        $is_require, $hints, $bitmask, $hinthash
+        $package,  $filename,   $line,  $subroutine, $hasargs, $wantarray,
+        $evaltext, $is_require, $hints, $bitmask,    $hinthash
     );
 }
 
